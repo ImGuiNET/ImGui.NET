8ce50e63a38c6973649d0c4883aedd9356883386
Eric Mellino <erme@microsoft.com>
2018-10-11 01:14:44 -0700

    Add several more manual ImGui overloads to ease the upgrade.

--- src/ImGui.NET/ImGui.Manual.cs
+++ src/ImGui.NET/ImGui.Manual.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace ImGuiNET
 {
@@ -9,7 +10,7 @@ namespace ImGuiNET
             byte[] buf,
             uint buf_size)
         {
-            return InputText(label, buf, buf_size, 0, null, null);
+            return InputText(label, buf, buf_size, 0, null, IntPtr.Zero);
         }
 
         public static bool InputText(
@@ -18,7 +19,7 @@ namespace ImGuiNET
             uint buf_size,
             ImGuiInputTextFlags flags)
         {
-            return InputText(label, buf, buf_size, flags, null, null);
+            return InputText(label, buf, buf_size, flags, null, IntPtr.Zero);
         }
 
         public static bool InputText(
@@ -28,7 +29,7 @@ namespace ImGuiNET
             ImGuiInputTextFlags flags,
             ImGuiInputTextCallback callback)
         {
-            return InputText(label, buf, buf_size, flags, callback, null);
+            return InputText(label, buf, buf_size, flags, callback, IntPtr.Zero);
         }
 
         public static bool InputText(
@@ -37,7 +38,7 @@ namespace ImGuiNET
             uint buf_size,
             ImGuiInputTextFlags flags,
             ImGuiInputTextCallback callback,
-            void* user_data)
+            IntPtr user_data)
         {
 
             int labelByteCount = Encoding.UTF8.GetByteCount(label);
@@ -49,8 +50,73 @@ namespace ImGuiNET
 
             fixed (byte* bufPtr = buf)
             {
-                return ImGuiNative.igInputText(labelBytes, bufPtr, buf_size, flags, callback, user_data) != 0;
+                return ImGuiNative.igInputText(labelBytes, bufPtr, buf_size, flags, callback, user_data.ToPointer()) != 0;
             }
+        }
+
+        public static bool InputText(
+            string label,
+            IntPtr buf,
+            uint buf_size)
+        {
+            return InputText(label, buf, buf_size, 0, null, IntPtr.Zero);
+        }
+
+        public static bool InputText(
+            string label,
+            IntPtr buf,
+            uint buf_size,
+            ImGuiInputTextFlags flags)
+        {
+            return InputText(label, buf, buf_size, flags, null, IntPtr.Zero);
+        }
+
+        public static bool InputText(
+            string label,
+            IntPtr buf,
+            uint buf_size,
+            ImGuiInputTextFlags flags,
+            ImGuiInputTextCallback callback)
+        {
+            return InputText(label, buf, buf_size, flags, callback, IntPtr.Zero);
+        }
+
+        public static bool InputText(
+            string label,
+            IntPtr buf,
+            uint buf_size,
+            ImGuiInputTextFlags flags,
+            ImGuiInputTextCallback callback,
+            IntPtr user_data)
+        {
+
+            int labelByteCount = Encoding.UTF8.GetByteCount(label);
+            byte* labelBytes = stackalloc byte[labelByteCount];
+            fixed (char* labelPtr = label)
+            {
+                Encoding.UTF8.GetBytes(labelPtr, label.Length, labelBytes, labelByteCount);
+            }
+
+            return ImGuiNative.igInputText(labelBytes, (byte*)buf.ToPointer(), buf_size, flags, callback, user_data.ToPointer()) != 0;
+        }
+
+        public static bool Begin(string name, ImGuiWindowFlags flags)
+        {
+            int name_byteCount = Encoding.UTF8.GetByteCount(name);
+            byte* native_name = stackalloc byte[name_byteCount + 1];
+            fixed (char* name_ptr = name)
+            {
+                int native_name_offset = Encoding.UTF8.GetBytes(name_ptr, name.Length, native_name, name_byteCount);
+                native_name[native_name_offset] = 0;
+            }
+            byte* p_open = null;
+            byte ret = ImGuiNative.igBegin(native_name, p_open, flags);
+            return ret != 0;
+        }
+
+        public static bool MenuItem(string label, bool enabled)
+        {
+            return MenuItem(label, string.Empty, false, enabled);
         }
     }
 }
